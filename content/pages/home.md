65c529ac82437fa38a7b69f0729aaf83451d2636
Colin McLear <mclear@fastmail.com>
2016-05-27 22:04:56 -0400

    Add website files

---
+++ content/pages/home.md
@@ -0,0 +1,15 @@
+TITLE: Welcome!
+URL: phil101.colinmclear.net
+SAVE_AS: index.html
+
+This is the homepage for PHIL 101 -- Introduction to Philosophy. 
+
+The subject matter of philosophy traditionally concerns the articulation of
+”big” questions such as, ”Can we know anything?”; ”What is real?”; ”What kinds
+of things exist?”. Philosophers use rigorous and disciplined methods of
+thinking, in the hopes of bringing such questions under intellectual control.
+This course provides an in-depth look at a few specific topics, rather than a
+comprehensive survey of the field and its history. You will learn what
+philosophy is all about and how to do it yourself.
+
+If you'd like to take a look at the syllabus you can find it here: [HTML]({filename}/extra/syllabus.html) | [PDF]({filename}/extra/syllabus.pdf)
---
+++ pelicanconf.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Colin McLear'
+SITENAME = u'Philosophy 101'
+SITEURL = 'phil101.colinmclear.net'
+TIMEZONE = 'America/New_York'
+DEFAULT_LANG = u'en'
+PATH = 'content'
+FAVICON = 'images/favicon.ico'
+READERS = {'html': None}
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# CACHE SETTINGS ########
+# CACHE_CONTENT = True
+# CACHE_PATH = '/Users/Roambot/Dropbox/Personal/bin/phil101/cache'
+# AUTORELOAD_IGNORE_CACHE = True
+# LOAD_CONTENT_CACHE = True
+# CHECK_MODIFIED_METHOD = 'mtime'
+
+
+# THEME SETTINGS ########
+THEME = 'pelican-bootstrap3'
+BOOTSTRAP_THEME='simplex'
+BOOTSTRAP_NAVBAR_INVERSE = True
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_ARTICLE_INFO_ON_INDEX = True
+DISPLAY_PAGES_ON_MENU = False
+MENUITEMS = (
+    ('Resources', '/resources'),
+    ('Schedule', '/schedule'),
+    ('Contact', '/contact'),
+    # ('Syllabus', '/syllabus'),
+    # ('Tags', '/tags.html'),
+    # ('Home', '/'),
+    # ('Category1', 'category/category1.html'),
+    # ('Category2', 'category/category2.html'),
+)
+# Turn on Typogrify
+TYPOGRIFY = True
+CC_LICENSE = "CC-BY-NC-SA"
+GOOGLE_ANALYTICS = 'UA-30497236-1'
+CUSTOM_CSS = 'static/custom.css'
+ARTICLE_PATHS = ['assignments']
+ARTICLE_SAVE_AS = 'assignments/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html' 
+ARTICLE_URL = 'assignments/{date:%Y}/{date:%b}/{date:%d}/{slug}' 
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+STATIC_PATHS = ['images', 'readings', 'themes', 'extra/CNAME', 'extra/custom.css', 'extra/syllabus.html', 'extra/syllabus.pdf']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, 'extra/custom.css': {'path': 'static/custom.css'}, 'extra/style.css': {'path': 'static/style.css'}, 'extra/syllabus.html': {'path': 'static/syllabus.html'}, 'extra/syllabus.pdf': {'path': 'static/syllabus.pdf'}}
+
+# Plugins ##########
+PLUGIN_PATHS = ['/Users/Roambot/bin/pelican-plugins']
+PLUGINS = ['org_pandoc_reader', 'assets', 'tipue_search', 'tag_cloud', 'neighbors', 'extract_toc']
+
+ORG_PANDOC_ARGS = [
+    '--filter=/usr/local/bin/pandoc-citeproc',
+    '--base-header-level=2',
+    '--bibliography=/Users/Roambot/Dropbox/Work/Master.bib',
+    '--mathjax',
+    '--toc',
+    '--toc-depth=5',
+  ]
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Tags
+
+TAG_CLOUD = True
+DISPLAY_TAGS_ON_SIDEBAR= True
+DISPLAY_TAGS_INLINE = True
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 100
+# TAG_CLOUD_SORTING = 'random'
+
+
+# Blogroll
+LINKS = (
+        ('Blackboard', 'https://my.unl.edu/webapps/portal/frameset.jsp'),
+        ('UNL Philosophy', 'http://www.unl.edu/philosophy/'))
+
+# Social widget
+# SOCIAL = (
+#           ('twitter', 'http://twitter.com/mclearc'),
+#           ('github', 'https://github.com/mclearc'),
+#           ('facebook', 'https://www.facebook.com/?_rdr=p'),
+#           )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
